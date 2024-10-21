8de0e444c5b6dddf31db6bb1db5d0ba281f42218
jel084 <130106981+jel084@users.noreply.github.com>
2024-10-21 14:59:58 -0700

    exercise 1 and 2

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,11 +1,17 @@
 import { Expense } from "../../types/types";
+import { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
+  const { expenses, setExpenses } = useContext(AppContext);
 
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
+    setExpenses(expenses.filter(expense => expense.id !== currentExpense.id))
   };
+  //  setfavorites(favorites.filter((id) => id !== noteId.toString()))
+
 
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
