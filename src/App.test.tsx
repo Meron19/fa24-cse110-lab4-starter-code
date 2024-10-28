0e4c7688dc3d59c2950606cef094bdaf58144e86
Meron19 <meronamershaoo@gmail.com>
2024-10-28 13:25:22 -0700

    testing branch: should be wrong

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Raul Sucks/i);
+  const linkElement = screen.getByText(/Dang Sucks/i);
   expect(linkElement).toBeInTheDocument();
 });
